2ab20ed8a35b2de11f995c490683e9232ddcb231
taehoon_park <taehoon_park@linecorp.com>
2022-03-12 16:02:45 +0900

    feat: hide keyboard when outside of EditText is touched

--- app/src/main/java/com/example/dgtimer/activities/main/MainActivity.java
+++ app/src/main/java/com/example/dgtimer/activities/main/MainActivity.java
@@ -2,19 +2,23 @@ package com.example.dgtimer.activities.main;
 
 import static com.example.dgtimer.ApplicationClass.refreshData;
 
+import android.content.Context;
+import android.graphics.Rect;
+import android.net.ConnectivityManager;
+import android.net.Network;
+import android.os.Bundle;
+import android.os.Parcelable;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.RecyclerView;
-
-import android.content.Context;
-import android.net.ConnectivityManager;
-import android.net.Network;
-import android.os.Bundle;
-import android.os.Parcelable;
-import android.view.View;
 
 import com.example.dgtimer.AppRater;
 import com.example.dgtimer.R;
@@ -44,7 +48,7 @@ public class MainActivity extends AppCompatActivity {
         binding.rvCapsules.addOnScrollListener(new RecyclerView.OnScrollListener() {
             @Override
             public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
-                viewModel.setScrollY(viewModel.getScrollY().getValue()+dy);
+                viewModel.setScrollY(viewModel.getScrollY().getValue() + dy);
             }
         });
 
@@ -52,8 +56,9 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onChanged(List<Capsule> capsules) {
                 Parcelable recyclerViewState = binding.rvCapsules.getLayoutManager().onSaveInstanceState();
-                viewModel.refreshData((CapsuleAdapter)binding.rvCapsules.getAdapter(), capsules);
-                if(recyclerViewState != null) binding.rvCapsules.getLayoutManager().onRestoreInstanceState(recyclerViewState);
+                viewModel.refreshData((CapsuleAdapter) binding.rvCapsules.getAdapter(), capsules);
+                if (recyclerViewState != null)
+                    binding.rvCapsules.getLayoutManager().onRestoreInstanceState(recyclerViewState);
             }
         });
 
@@ -65,6 +70,28 @@ public class MainActivity extends AppCompatActivity {
         });
 
         setNetworkCallback();
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        if (ev.getAction() == MotionEvent.ACTION_DOWN) {
+            View view = getCurrentFocus();
+            if (view instanceof EditText) {
+                Rect searchRect = new Rect();
+                binding.llSearch.getGlobalVisibleRect(searchRect);
+                if (!searchRect.contains((int) ev.getRawX(), (int) ev.getRawY())) {
+                    view.clearFocus();
+                    hideKeyboard(view);
+                }
+            }
+        }
+
+        return super.dispatchTouchEvent(ev);
+    }
+
+    private void hideKeyboard(View view) {
+        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
     }
 
     private void setNetworkCallback() {
@@ -93,17 +120,17 @@ public class MainActivity extends AppCompatActivity {
     }
 
     //fab onClick
-    public void scrollUp(View view){
+    public void scrollUp(View view) {
         binding.rvCapsules.smoothScrollToPosition(0);
     }
 
     //끝내기 전에 평가 dialog 띄우기
     @Override
     public void onBackPressed() {
-        if(viewModel.getSearchOn().getValue()){
+        if (viewModel.getSearchOn().getValue()) {
             binding.etSearch.setText("");
             viewModel.getSearchOn().setValue(false);
-        }else{
+        } else {
             AppRater.set(this);
         }
     }
