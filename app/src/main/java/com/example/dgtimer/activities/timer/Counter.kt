6d7f296c5ecc61290fc8dc74dbfe052983e45ced
taehoon_park <taehoon_park@linecorp.com>
2022-07-10 18:46:59 +0900

    refactor: add KCounterView

---
+++ app/src/main/java/com/example/dgtimer/activities/timer/Counter.kt
@@ -0,0 +1,8 @@
+package com.example.dgtimer.activities.timer
+
+data class Counter(
+    val type: String,
+    val currentTime: Int,
+    val index: Int,
+    val isActive: Boolean
+)
---
+++ app/src/main/java/com/example/dgtimer/activities/timer/KCounterView.kt
@@ -0,0 +1,40 @@
+package com.example.dgtimer.activities.timer
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import androidx.core.content.ContextCompat
+import androidx.core.view.children
+import com.example.dgtimer.R
+import com.example.dgtimer.databinding.ViewCounterBinding
+
+class KCounterView(
+    context: Context,
+) : LinearLayout(context) {
+    private val binding: ViewCounterBinding =
+        ViewCounterBinding.inflate(LayoutInflater.from(context), this, true)
+
+    fun setCounter(counter: Counter) {
+        with(binding) {
+            tvCounterCount.text = counter.currentTime.toString()
+            tvCounterCount.setTextColor(getTextColorByActiveState(counter.isActive))
+            val counterViewCount =
+                (parent as ViewGroup).children.filter { it is KCounterView }.count()
+            tvCounterName.text =
+                if (counter.index == 0 && counterViewCount > 1) {
+                    context.getString(R.string.latte)
+                } else {
+                    counter.type
+                }
+            tvCounterName.setTextColor(getTextColorByActiveState(counter.isActive))
+        }
+    }
+
+    private fun getTextColorByActiveState(isActive: Boolean) =
+        if (isActive) {
+            ContextCompat.getColor(context, R.color.colorBackground)
+        } else {
+            ContextCompat.getColor(context, R.color.colorTextTransparent)
+        }
+}
