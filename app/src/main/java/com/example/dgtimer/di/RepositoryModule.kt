80b4a75cb8711648b0b1b392b33ad3228176caea
taehoon_park <taehoon_park@linecorp.com>
2022-08-07 17:05:45 +0900

    refactor: edit gradle & remove useless hilt modules

--- app/src/main/java/com/example/dgtimer/di/RepositoryModule.kt
+++ app/src/main/java/com/example/dgtimer/di/RepositoryModule.kt
@@ -1,6 +1,7 @@
 package com.example.dgtimer.di
 
 import com.example.dgtimer.repo.CapsuleRepository
+import com.example.dgtimer.repo.CapsuleRepositoryImpl
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -10,5 +11,5 @@ import dagger.hilt.components.SingletonComponent
 @Module
 interface RepositoryModule {
     @Binds
-    fun provideContractRepositoryImpl(repository: CapsuleRepository): CapsuleRepository
+    fun provideContractRepositoryImpl(repository: CapsuleRepositoryImpl): CapsuleRepository
 }
