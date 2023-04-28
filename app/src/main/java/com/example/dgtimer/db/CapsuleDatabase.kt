582ff866bdf8456d3d2a9cb67e8e92ea35b54b5c
taehoon-park <taehoon_park@linecorp.com>
2023-04-28 22:07:59 +0900

    chore: set exportSchema false on CapsuleDatabase room database annotation

--- app/src/main/java/com/example/dgtimer/db/CapsuleDatabase.kt
+++ app/src/main/java/com/example/dgtimer/db/CapsuleDatabase.kt
@@ -4,7 +4,7 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 
-@Database(entities = [Capsule::class], version = 1, exportSchema = true)
+@Database(entities = [Capsule::class], version = 1, exportSchema = false)
 @TypeConverters(Converter::class)
 abstract class CapsuleDatabase : RoomDatabase() {
     abstract fun capsuleDao(): CapsuleDao
