723fbf4bea7ad9b660cc9b6208342c65bbad27a5
taehoon-park <taehoon_park@linecorp.com>
2023-04-28 21:12:08 +0900

    refactor: add export schema option on room database annotation

--- app/src/main/java/com/example/dgtimer/db/CapsuleDatabase.kt
+++ app/src/main/java/com/example/dgtimer/db/CapsuleDatabase.kt
@@ -4,7 +4,7 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 
-@Database(entities = [Capsule::class], version = 1)
+@Database(entities = [Capsule::class], version = 1, exportSchema = true)
 @TypeConverters(Converter::class)
 abstract class CapsuleDatabase : RoomDatabase() {
     abstract fun capsuleDao(): CapsuleDao
